976d86578a7e3c6e26dbb184321530283bbcf04c
TJ Momon <tjmomon@TJs-MacBook-Air.local>
2023-02-09 00:30:11 -0600

    fixed bug where localStorage data did not persist through refresh

--- assets/script.js
+++ assets/script.js
@@ -7,20 +7,32 @@ function newCity() {
         console.log('Error: No data entered');
     } else {
         if (userEntry !== "") {
-            localStorage.setItem(userEntry, userEntry);
+            localStorage.setItem(userEntry, " ");
             document.querySelector(".city-data").textContent = " ";
         }
-        for (let i = 0; i< localStorage.length; i++) {
-            var b = document.createElement("button");
-            var city = localStorage.key(i);
-            b.textContent = city;
-            document.querySelector(".city-data").appendChild(b);
-        }
+        // for (let i = 0; i< localStorage.length; i++) {
+        //     var b = document.createElement("button");
+        //     var city = localStorage.key(i);
+        //     b.textContent = city;
+        //     document.querySelector(".city-data").appendChild(b);
+        // }
     }
 };
+
+function saveCity() {
+    for (let i = 0; i< localStorage.length; i++) {
+        var b = document.createElement("button");
+        var city = localStorage.key(i);
+        b.textContent = city;
+        document.querySelector(".city-data").appendChild(b);
+    }
+};
+
+saveCity();
 
 // eventListener on 'Search' button to save city search to localStorage
 searchButton.addEventListener("click", function(event) {
     event.preventDefault();
     newCity();
+    saveCity();
 });
