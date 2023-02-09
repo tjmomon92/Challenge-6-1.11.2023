073049c37037f2d757106cc084e94fe23e620be1
TJ Momon <tjmomon@TJs-MacBook-Air.local>
2023-02-08 20:25:36 -0600

    finalized all eventListeners for Search button

--- assets/script.js
+++ assets/script.js
@@ -7,15 +7,22 @@ function newCity() {
         userEntry = "anonymous";
     }
     localStorage.setItem(userEntry, userEntry);
+};
+
+// Function to populate city search from localStorage
+function printCity() {
     document.querySelector(".city-data").textContent = " ";
-    var p = document.createElement("p");
-    p.textContent = userEntry;
-    document.querySelector(".city-data").appendChild(p);
+    for (let i = 0; i< localStorage.length; i++) {
+        var p = document.createElement("p");
+        var city = localStorage.key(i);
+        p.textContent = city;
+        document.querySelector(".city-data").appendChild(p);
+    }
 };
 
 // eventListener on 'Search' button to save city search to localStorage
 searchButton.addEventListener("click", function(event) {
     event.preventDefault();
     newCity();
+    printCity();
 });
-
