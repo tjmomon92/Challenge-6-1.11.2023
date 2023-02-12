4e29874a02940c5d19cfab41b74b0012944f1b3a
TJ Momon <tjmomon@TJs-MacBook-Air.attlocal.net>
2023-02-12 15:18:00 -0600

    got API info in. Calls working, but getting a CORS error

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,62 @@
 const searchButton = document.querySelector(".search-btn");
-// const cityGeoSearch = document.getElementById('cityGeo');
+const cityGeoSearch = Object.entries(localStorage);
+
+console.log(cityGeoSearch);
 
 // Function to set user input to local storage
+// function newCity(parameter) {
 function newCity() {
     const userEntry = document.querySelector("#city-entry").value;
+    // if (parameter === "") {
     if (userEntry === "") {
         console.log('Error: No data entered');
+        return;
     } else {
+        // if (parameter !== "") {
         if (userEntry !== "") {
+            // localStorage.setItem(parameter, " ");
             localStorage.setItem(userEntry, " ");
             document.querySelector(".city-data").textContent = " ";
         }
-    }
+        //  fetch(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/geo/1.0/direct?q=${parameter}&limit=3&appid=d769864f4813769c7a82a33216c211e2`)
+         fetch(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/geo/1.0/direct?q=${userEntry}&limit=3&appid=d769864f4813769c7a82a33216c211e2`)
+             .then(data=>{return data.json()})
+             .then(response=> {
+                fetch(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?lat=${response[0].lat}&lon=${response[0].lon}&appid=d769864f4813769c7a82a33216c211e2`)
+                .then(finalResponse=>{
+                console.log(finalResponse)
+                //write dom stuff here
+                })
+            })
+     .catch(error => {
+         console.log(error);
+     })
+         }
+     let response;
 };
 
 // Function to save search history to page for easy access
-function saveCity() {
+function saveCity(parameter) {
     for (let i = 0; i< localStorage.length; i++) {
         const b = document.createElement("button");
         const city = localStorage.key(i);
+        // const city = localStorage.getItem('userEntry');
         b.textContent = city;
         document.querySelector(".city-data").appendChild(b);
-        b.setAttribute('id', 'cityGeo');
+        b.setAttribute('class', 'buttons');
     }
-};
+    document.querySelector(".city-data").addEventListener("click",function(event){
+        event.preventDefault;
+            newCity(event.target.innerText)
+            saveCity(event.target.innerText)
+        })
+    };
 
 // prevents search data from disappearing on page refresh
 saveCity();
 
 // eventListener on 'Search' button to save city search to localStorage
-searchButton.addEventListener("click", function(event) {
-    event.preventDefault();
-    newCity();
-    saveCity();
-});
-
-// Geocoding API to get Lat and Lon for future 5-Day Forecast call
-// const geoCode = () => {
-    fetch("https://api.openweathermap.org/geo/1.0/direct?q=dallas&limit=5&appid=d769864f4813769c7a82a33216c211e2")
-        .then(response => {
-            return response.json();
-        })
-        .then(response => {
-            console.log(response);
-        })
-        .catch(error => {
-            console.log(error);
-        })
-// }
+document.querySelector("#search-button").addEventListener("click",function(event){
+    newCity(event.target.previousElementSibling.value)
+    saveCity(event.target.innerText)
+})
